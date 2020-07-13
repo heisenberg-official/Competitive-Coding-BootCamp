744dc89a5f7ab4feb27cb2558541542356429290
Vineet Kumar <44500552+heisenberg-official@users.noreply.github.com>
2020-07-14 03:49:50 +0530

    Add files via upload
    Remove Duplicates Using Vector Most Efficient
    Runtime: 32 ms
    Memory: 13.7 MB

---
+++ EASY/[1]-ARRAYS/[1] Remove Duplicates from Sorted Array.cpp
@@ -0,0 +1,25 @@
+#include<algorithm>
+#include<iostream>
+#include<vector>
+
+class Solution 
+{
+public:
+    int removeDuplicates(std::vector<int>& nums) 
+    {
+        nums.erase(unique(nums.begin(),nums.end()),nums.end());
+        return nums.size();
+    }
+};
+
+int main()
+{
+    Solution A;
+    std::vector<int> x = { 0,0,1,1,1,2,2,3,3,4 };
+    A.removeDuplicates(x);
+
+    for (int i = 0; i < x.size(); i++)
+        std::cout << "\n" << x[i];
+
+    return 0;
+}
