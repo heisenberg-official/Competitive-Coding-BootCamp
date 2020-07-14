71858a9e3e7efdf51f0b233d41e200784872ba63
Vineet Kumar <44500552+heisenberg-official@users.noreply.github.com>
2020-07-14 20:38:19 +0530

    Add files via upload

---
+++ EASY/[1]-ARRAYS/[2] Best Time to Buy and Sell Stock II.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+class Solution
+{
+public:
+    int maxProfit(std::vector<int>& prices)
+    {
+        int profit = 0;
+
+        if (prices.size() == 0)
+            return 0;
+
+        for (int i = 0; i < prices.size() - 1; i++)
+        {
+            if (prices[i + 1] > prices[i])
+                profit += (prices[i + 1] - prices[i]);
+        }
+
+        return profit;
+    }
+};
+
+int main()
+{
+    Solution A;
+    std::vector<int> X{ 7,1,5,3,6,4 };
+    
+    A.maxProfit(X);
+
+	return 0;
+}
