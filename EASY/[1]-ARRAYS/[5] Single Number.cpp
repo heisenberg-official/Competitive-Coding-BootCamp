eaf649b8bd9e776d8fccd9997ef7d8eccf3b85ff
Vineet Kumar <44500552+heisenberg-official@users.noreply.github.com>
2020-07-17 00:14:15 +0530

    Update [5] Single Number.cpp

--- EASY/[1]-ARRAYS/[5] Single Number.cpp
+++ EASY/[1]-ARRAYS/[5] Single Number.cpp
@@ -8,45 +8,19 @@ public:
         cout.tie(NULL);
     }
     
-    int singleNumber(std::vector<int>& nums)
+    int singleNumber(vector<int>& nums) 
     {
-        sort(nums.begin(), nums.end());
+       int x = 0;
+       for (int i = 0; i < nums.size(); i++)
+           x^= nums[i];
         
-        if(nums.size()==1)
-            return nums[0];
-        
-        if (nums[0] != nums[1])
-            return nums[0];
-        
-        int x = 1, y = 1;
-        bool flag = true;
-        
-        for (int i = 0; i < nums.size(); i++)
-        {
-            if (flag)
-            {
-                if (nums[i] == nums[i + 1])
-                    x = 1;
-                else
-                    x = 0;
-            }
-            else
-            {
-                if (nums[i] == nums[i + 1])
-                    y = 1;
-                else
-                    y = 0;
-
-            }
-
-            if (flag)
-                flag = false;
-            else
-                flag = true;
-            
-            if (x == 0 && y == 0)
-                return nums[i];
-        }
-        return 0;
+        return x;
     }
-};
+
+int main()
+{
+    Solution A;
+    std::vector<int> X{ 4,1,2,1,2 };
+    std::cout << A.singleNumber(X);
+    return 0;
+}
