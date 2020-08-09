29d9c557a5490779ba310414041a25c7be392e6b
Vineet Kumar <44500552+heisenberg-official@users.noreply.github.com>
2020-08-09 18:55:28 +0530

    Explanation Part 1

--- EASY/[3]-Linked-List/[0] Linked List Add From End.cpp
+++ EASY/[3]-Linked-List/[0] Linked List Add From End.cpp
@@ -2,25 +2,27 @@
 
 struct node
 {
-	int data;
-	node* next;
-};	node* head = NULL;
+	int data;  // Integer Data For Node
+	node* next; // Pointer ( Helps To Point At Another Node In Memory )
+};
+
+node* head = NULL; // Initialize head element for the starting of the linked list
 
 void insert(int x)
 {
-	node* ptr;
-	node* new_node = (node*)malloc(sizeof(node)); 
+	node* ptr; // Temporary pointer used in else condition 
+	node* new_node = (node*)malloc(sizeof(node)); // New temporary node for head and other nodes data assigning
 
-	new_node->data = x;
-	new_node->next = NULL;
+	new_node->data = x;  // Integer X stored in new node
+	new_node->next = NULL; // Next PTR of new node Pointing To Null ( TAIL Of The L.L) 
 
 	if (head == NULL)
-		head = new_node;
+		head = new_node; // if head is null then assign new node as first element in head
 	else
 	{
-		ptr = head;
-		while (ptr->next!=NULL)
-			ptr = ptr->next;
+		ptr = head; // if head is not null and contatains some element point PTR temp to head pointer for traverse using next while loop
+		while (ptr->next!=NULL) // if the current element next is pointing to null then its the last element, And ptr of next will be assigned to new_node in LINE 26
+			ptr = ptr->next; // if not true traverse to next element and check again
 		ptr->next = new_node;
 	}
 }
