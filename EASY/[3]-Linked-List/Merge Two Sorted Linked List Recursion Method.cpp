b58f341802f94ceb8b97b1d92be4d10a81bc7b2c
Vineet Kumar <44500552+heisenberg-official@users.noreply.github.com>
2020-08-13 17:22:11 +0530

    Create Merge Two Sorted Linked List Recursion Method.cpp

---
+++ EASY/[3]-Linked-List/Merge Two Sorted Linked List Recursion Method.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+
+struct ListNode 
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution 
+{
+public:
+    Solution()
+    {
+        std::ios_base::sync_with_stdio(false);
+        std::cin.tie(0);
+    }
+    ListNode* mergeTwoLists(ListNode* x1, ListNode* x2) 
+    {
+        if(!x1)
+            return x2;
+        if(!x2)
+            return x1;
+        ListNode* head = x1->val <= x2->val ? x1 : x2;
+        head->next = x1->val <= x2->val ? mergeTwoLists(x1->next,x2) : mergeTwoLists(x1,x2->next); //Using Recursive Method To 
+        return head;
+    }
+};
